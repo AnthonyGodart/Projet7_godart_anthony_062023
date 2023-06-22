37b26da69eaf781088dc4b7431c4aa0de40191ac
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-06-22 16:39:58 +0200

    corrected CORS conflict with Helmet

--- app.js
+++ app.js
@@ -16,17 +16,21 @@ mongoose.connect('mongodb+srv://testuser:TestUS3R@cluster.4srddnz.mongodb.net/?r
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}));
+
 app.use(mongoSanitize());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 
