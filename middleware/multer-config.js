26d7fc33b8c84619b0fa377f72ac82bcc88a20ca
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-06-24 00:34:50 +0200

    corrected bug while modifying book

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -12,7 +12,7 @@ const MIME_TYPES = {
 const mimeTypes = Object.keys(MIME_TYPES);
 const extensions = Object.values(MIME_TYPES);
 const fileFilter = (req, file, callback) => {
-  if (mimeTypes.includes(file.mimetype) && extensions.includes(file.extension)) {
+  if (mimeTypes.includes(file.mimetype) && extensions.includes(path.extname(file.originalname).toLowerCase())) {
     callback(null, true);
   } else {
     callback(new Error('Le fichier doit être une image.'), false);
@@ -31,26 +31,32 @@ const storage = multer.memoryStorage({
 });
 
 module.exports = (req, res, next) => {
-  const middlewareMulter = multer({storage}).single('image');
+  const middlewareMulter = multer({ storage }).single('image');
 
-  const customNext = () => {
+  middlewareMulter(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(400).json({ error: err.message });
+    } else if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (req.file === undefined) {
+      return next();
+    }
+
     const buffer = req.file.buffer;
     const outputPath = path.join(__dirname, '..', 'images', path.parse(req.file.originalname).name.split(' ').join('_') + Date.now() + '.webp');
     sharp(buffer)
-        .rotate()
-        .resize(null, 600)
-        .webp({ quality: 20 })
-        .toFile(outputPath)
-        .then(() => {
-          req.file.originalname = path.basename(outputPath);
-          next(); 
-        })
-        .catch(error => { 
-          next(error); 
-        });
-  };
-
-  middlewareMulter.fileFilter = fileFilter;
-
-  middlewareMulter(req, res, customNext);
+      .rotate()
+      .resize(null, 600)
+      .webp({ quality: 20 })
+      .toFile(outputPath)
+      .then(() => {
+        req.file.originalname = path.basename(outputPath);
+        next();
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
 };
