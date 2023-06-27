2ccaee528095ca37fb4b82e94b728a26ab525653
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-06-27 14:31:11 +0200

    starting creating Swagger documentation

--- app.js
+++ app.js
@@ -6,8 +6,34 @@ const mongoose = require('mongoose');
 const booksRoutes = require('./routes/books');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
+
+/* Configuration de Swagger */
+// Définition des options Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentation for your API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000', // Remplacez par l'URL de votre backend
+      },
+    ],
+  },
+  apis: ['./routes/books.js'], // Chemin vers les fichiers contenant les annotations Swagger
+};
+// Générer la spécification Swagger
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+// Afficher la documentation Swagger via Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+/* Fin de configuration de Swagger */
 
 mongoose.connect('mongodb+srv://testuser:TestUS3R@cluster.4srddnz.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -16,11 +42,13 @@ mongoose.connect('mongodb+srv://testuser:TestUS3R@cluster.4srddnz.mongodb.net/?r
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
+
 app.use(helmet({
     crossOriginResourcePolicy: false,
 }));
-
 app.use(mongoSanitize());
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
@@ -31,8 +59,6 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
-
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/books', booksRoutes);
--- routes/books.js
+++ routes/books.js
@@ -4,12 +4,192 @@ const fileHandler = require('../middleware/multer-config');
 const booksCtrl = require('../controllers/books');
 const router = express.Router();
 
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     description: Create a new book
+ *     tags:
+ *       - Books
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/BookInput'
+ *     responses:
+ *       201:
+ *         description: Book created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ */
 router.post('/', auth, fileHandler, booksCtrl.createBook);
+
+/**
+ * @swagger
+ * /books/bestrating:
+ *   get:
+ *     summary: Get top-rated books
+ *     description: Get a list of top-rated books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
 router.get('/bestrating', booksCtrl.getTopRatedBooks);
+
+/**
+ * @swagger
+ * /books/{id}/rating:
+ *   post:
+ *     summary: Rate a book
+ *     description: Rate a book
+ *     tags:
+ *       - Books
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the book to rate
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RatingInput'
+ *     responses:
+ *       201:
+ *         description: Rating added successfully
+ *       400:
+ *         description: Invalid request body
+ *       404:
+ *         description: Book not found
+ */
 router.post('/:id/rating', auth, booksCtrl.rateBook);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   put:
+ *     summary: Update a book
+ *     description: Update a book by ID
+ *     tags:
+ *       - Books
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the book to update
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/BookInput'
+ *     responses:
+ *       200:
+ *         description: Book updated successfully
+ *       400:
+ *         description: Invalid request body
+ *       401:
+ *         description: Not authorized
+ *       404:
+ *         description: Book not found
+ */
 router.put('/:id', auth, fileHandler, booksCtrl.modifyBook);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   delete:
+ *     summary: Delete a book
+ *     description: Delete a book by ID
+ *     tags:
+ *       - Books
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the book to delete
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book deleted successfully
+ *       401:
+ *         description: Not authorized
+ *       404:
+ *         description: Book not found
+ */
 router.delete('/:id', auth, booksCtrl.deleteBook);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Get a book by ID
+ *     description: Get a book by ID
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the book to retrieve
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: Book not found
+ */
 router.get('/:id', booksCtrl.getOneBook);
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     description: Get a list of all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
 router.get('/', booksCtrl.getAllBooks);
 
 module.exports = router;
